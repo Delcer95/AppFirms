af0aea815c14f629359381142410faff0ead2d87
Delcer Hernandez <hernandezdelcer@gmail.com>
2024-03-24 02:17:53 -0600

    Initial commit

---
+++ app/src/main/java/com/example/appfirms/Config/Asignaturess.java
@@ -0,0 +1,36 @@
+package com.example.appfirms.Config;
+
+public class Asignaturess {
+    private byte[] signature;
+    private String id, nombre;
+
+    public Asignaturess(byte[] signature, String id, String nombre) {
+        this.signature = signature;
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public byte[] getSignature() {
+        return signature;
+    }
+
+    public void setSignature(byte[] signature) {
+        this.signature = signature;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
