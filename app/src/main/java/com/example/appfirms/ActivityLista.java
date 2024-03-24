5f75874ce95507b95692288d6bb340819fa7dac2
Delcer Hernandez <hernandezdelcer@gmail.com>
2024-03-24 02:30:47 -0600

    Layout

---
+++ app/src/main/java/com/example/appfirms/ActivityLista.java
@@ -0,0 +1,71 @@
+package com.example.appfirms;
+
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.appfirms.Config.Asignaturess;
+import com.example.appfirms.Config.DataBaseHelper;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActivityLista extends AppCompatActivity {
+
+    ListView listView;
+    List<Asignaturess> Firmas = new ArrayList<>();
+    ListAdapter mAdapter;
+    DataBaseHelper conexion;
+    FloatingActionButton btnRegresar;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registros);
+
+        conexion = new DataBaseHelper(this);
+
+        btnRegresar = findViewById(R.id.btnRegresar);
+        btnRegresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed(); // Vuelve a la actividad anterior al presionar el botón
+            }
+        });
+
+
+        listView = (ListView) findViewById(R.id.listView);
+        obtenerTabla();
+        mAdapter = new ListAdapter(this,Firmas);
+        listView.setAdapter(mAdapter);
+
+    }
+
+    private void obtenerTabla() {
+
+        SQLiteDatabase db = conexion.getReadableDatabase();
+        Firmas firmas = null;
+        //Cursor de base de datos
+        Cursor cursor = db.rawQuery(DataBaseHelper.SelectTable,null);
+
+        //Recorremos el cursor
+        while (cursor.moveToNext()){
+            firmas = new Firmas();
+            firmas.setId(cursor.getString(0));
+            firmas.setNombre(cursor.getString(2));
+            Firmas.add(firmas);
+        }
+        cursor.close();
+    }
+
+
+
+}
--- app/src/main/java/com/example/appfirms/MainActivity.java
+++ app/src/main/java/com/example/appfirms/MainActivity.java
@@ -1,6 +1,14 @@
 package com.example.appfirms;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +16,75 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.appfirms.Config.DataBaseHelper;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.ByteArrayOutputStream;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mainBinding;
+
+    private DataBaseHelper dbHelper;
+    EditText edtNombre;
+    FloatingActionButton btnLimpiar;
+    Button btnSalvados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+        edtNombre=findViewById(R.id.edtNombre);
+        btnLimpiar= findViewById(R.id.btnLimpiar);
+        btnSalvados = findViewById(R.id.btnFirmasSalvados);
+        mainBinding.btnLimpiar.setOnClickListener(view ->{
+            mainBinding.signatureView.clearCanvas();
         });
+
+        dbHelper = new DataBaseHelper(this);
+
+        mainBinding.btnSalvarFirma.setOnClickListener(view -> {
+            Bitmap signBitmap = mainBinding.signatureView.getSignatureBitmap();
+            if (signBitmap != null) {
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                signBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+                // Aquí debes insertar el byte array en la base de datos utilizando SQLiteOpenHelper o algún otro método de acceso a la base de datos
+                // Por ejemplo:
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("signature", byteArray);
+                values.put("nombre", edtNombre.getText().toString());
+                long result = db.insert(DataBaseHelper.tableName, null, values);
+
+                if (result != -1) {
+                    Toast.makeText(MainActivity.this, "Firma guardada correctamente", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Error al guardar la firma", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+
+
+
+        btnSalvados.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ActivityLista.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+    @Override
+    protected void onDestroy() {
+        dbHelper.close();
+        super.onDestroy();
+    }
+
+
+
 }
